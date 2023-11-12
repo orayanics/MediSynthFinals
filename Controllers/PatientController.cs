dca9e503e16565f1b6b9f0c3e9f4f4a50024f1e0
orayanics <nicole.oraya.cics@ust.edu.ph>
2023-11-12 20:29:15 +0800

    db patient and related records

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,46 @@
+﻿using MediSynthFinals.Data;
+using MediSynthFinals.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace MediSynthFinals.Controllers
+{
+    public class PatientController : Controller
+    {
+        // Db Context
+        private readonly MediDbContext _dbContext;
+
+        public PatientController(MediDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(_dbContext.PatientCredentials);
+        }
+
+        public IActionResult Details(int id)
+        {
+            PatientCredentials? patient = _dbContext.PatientCredentials.FirstOrDefault(x => x.patientId == id);
+
+            if (patient != null)
+            {
+                dynamic model = new ExpandoObject();
+                model.PatientCredentials = _dbContext.PatientCredentials;
+                model.RecordDiagnosis = _dbContext.RecordDiagnosis;
+                model.RecordMedHistory = _dbContext.RecordMedHistory;
+                {
+                    if (model != null)
+                    {
+                        ViewBag.PatientId = id;
+                        return View(model);
+                    }
+                }
+            }
+
+            return NotFound();
+        }
+    }
+}
